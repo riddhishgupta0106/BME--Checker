c7006cc94ccb095d87d8a143b5ec2a4012054302
riddhish <riddhish01062004@gmail.com>
2026-06-01 12:25:33 +0530

    Update dashboard configuration

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/BME--Checker/",
-  plugins: [react()]
+  base: "/airport-resource-dashboard/",
+  plugins: [react()],
 });
